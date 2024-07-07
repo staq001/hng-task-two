181df3f9464358beaa2aa8acee1982f3dd6e7039
staq001 <dame.hush@gmail.com>
2024-07-07 20:54:24 +0100

    dialectModule

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,5 +1,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize(`${process.env.PSQL_URL}`)
+const sequelize = new Sequelize(`${process.env.PSQL_URL}`, {
+  dialectModule: require("pg")
+})
 
 module.exports = { sequelize, DataTypes };
